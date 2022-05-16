cc4d88eab9c89eb3290ee5c1729d9b4c4c3fc391
Majdi Hajjem <hajjem.mejdi@gmail.com>
2022-05-16 19:26:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import "./App.css";
+export default class App extends Component {
+  state={
+    person:{
+      fullName: 'Majdi',
+      bio :'Bio',
+      imgsrc:'./avatar.jpg',
+      profession:'Student'},
+      show:false,
+      count:0
+    }
+    
+    render() {
+    return (
+      <div className='App'>
+        {this.state.show ? <div> <img src={this.state.person.imgsrc} style={{height:'200px',width:'200px'}} /><h1>{this.state.person.fullName}</h1> <h2>{this.state.person.bio}</h2> <h2>{this.state.person.profession}</h2></div>:null}
+        <button onClick={()=>this.setState({show:!this.state.show})}>Show</button>
+        <h1 style={{color:'red'}}>Life Cycle</h1>
+        <h1 style={{color:'red'}}>{this.state.count}</h1>
+      </div>
+    )
+  }
+  componentDidMount  (){
+    console.log("safta")
+    setInterval(()=>{this.setState({count:this.state.count+1})},1000)
+  }
 }
-
-export default App;
